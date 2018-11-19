cfaf8ab4bb2cf4ea5888d64e2878b73e73bb7b89
David Arens <davidarens@me.com>
2018-11-19 06:51:14 +0100

    mvp app improved

--- map-the-solar-system/server.R
+++ map-the-solar-system/server.R
@@ -9,12 +9,13 @@
 
 library(shiny)
 library(leaflet)
+library(lubridate)
 library(moonsun)
 library(geosphere)
+library(data.table)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    observeEvent(input$submitValues, {
     planetNames <- c("sun", 
                      "mercury",
                      "venus",
@@ -25,24 +26,28 @@ shinyServer(function(input, output) {
                      "neptune",
                      "pluto")
     
-    planetsAngles <- (-1) * (as.numeric(as.ecc(planets(show.moon = FALSE))$lat) - 360) #To get the top-down-view from the ecliptic north to south poles
+    #To get the top-down-view from the ecliptic north to south poles
+    observationDate <- reactive({as.Date(input$date)})
+    planetScale <- reactive({as.numeric(input$kmPerAU)})
     
-    planetScale <- input$kmPerAU
+    planetsAngles <- reactive({(-1) * (as.numeric(as.ecc(planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE))$lat) - 360)})
+    planetsDistances <- reactive({planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE)$dist * planetScale() * 1000})
+    planetsSize <- 2 * c(695508, 2439.7, 6051.8, 3396.2, 71492, 60268, 25559, 24764, 1195) # Source: NASA, radii converted to diameter
+        #Earth	6378.1
     
-    planetsDistances <- planets(show.moon = FALSE)$dist * planetScale * 1000
+    startingLocationLng <- reactive({as.numeric(input$startingLocationLng)})
+    startingLocationLat <- reactive({as.numeric(input$startingLocationLat)})
     
-    startingLocationLng <- as.numeric(input$startingLocationLng)
-    startingLocationLat <- as.numeric(input$startingLocationLat)
+    newCoords <- reactive({destPoint(c(startingLocationLng(), startingLocationLat()), planetsAngles(), planetsDistances())})
     
-    newCoords <- destPoint(c(startingLocationLng, startingLocationLat), planetsAngles, planetsDistances)
-    
-    
-  
     output$solarSystemMap <- renderLeaflet({
         leaflet() %>%
         addTiles() %>%
-        addMarkers(lng = startingLocationLng, lat = startingLocationLat) %>%
-        addMarkers(lng = newCoords[,1], lat = newCoords[,2], popup = planetNames)
-  })
+        addMarkers(lng = startingLocationLng(), lat = startingLocationLat(), label = "earth") %>%
+        addMarkers(lng = newCoords()[,1], lat = newCoords()[,2], label = planetNames)
+    })
+    
+    output$coordinatesDT <- renderDataTable({
+        data.table(Planet = planetNames, Longitude = newCoords()[,1], Latitude = newCoords()[,2], "Distance in AU" = planetsDistances() / ( 1000 * planetScale()), "Diameter in km"  = planetsSize)
     })
 })
--- map-the-solar-system/ui.R
+++ map-the-solar-system/ui.R
@@ -10,25 +10,41 @@
 library(shiny)
 library(leaflet)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Map the Solar System Around You"),
   
-  # Sidebar with a slider input for number of bins 
+  #Sidebar layout with setting on the left and a map output to the right
   sidebarLayout(
     sidebarPanel(
        dateInput("date", "Date of observation"),
        textInput("startingLocationLat", "Latitude, e.g. 35.0992539", value = "35.0992539"),
        textInput("startingLocationLng", "Longitute, e.g. -115.7863069", value = "-115.7863069"),
-       numericInput("kmPerAU", "How much should 1 AU be in km?", value = 0.5, min = 0.01, max = 10, step = 0.01),
-       actionButton("submitValues", "Apply to map")
+       sliderInput("kmPerAU", "How much should 1 AU be in km?", value = 0.5, min = 0.01, max = 10, step = 0.01)
     ),
     
-    # Show a plot of the generated distribution
+    # Show a map of the location of stars relative to earth on a give day, with earth being at your position
     mainPanel(
-        leafletOutput("solarSystemMap")
+        tabsetPanel(
+            tabPanel("mapPanel",
+                     img(src="earth.png"),
+                     leafletOutput("solarSystemMap")),
+            tabPanel("dataPanel",
+                     dataTableOutput("coordinatesDT"))
+        )
     )
   )
 ))
+
+# images:
+#     earth: http://pngimg.com/download/25352
+#     sun: http://pngimg.com/download/13424
+#     mars: https://commons.wikimedia.org/wiki/File:3D_Mars.png
+#     venus: https://commons.wikimedia.org/wiki/File:3D_Venus.png
+#     uranus: https://commons.wikimedia.org/wiki/File:3D_Uranus.png
+#     jupiter: https://commons.wikimedia.org/wiki/File:3D_Jupiter.png
+#     mercury: https://commons.wikimedia.org/wiki/File:3D_Mercury.png
+#     saturn: https://commons.wikimedia.org/wiki/File:3D_Saturn.png
+#     neptune: https://commons.wikimedia.org/wiki/File:3D_Neptune.png
+#     pluto: https://commons.wikimedia.org/wiki/File:3D_Pluto.png
