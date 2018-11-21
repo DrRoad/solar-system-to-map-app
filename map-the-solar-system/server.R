1eedb22b6766200c8d40eeca317d668e67cfb7ae
David Arens <davidarens@me.com>
2018-11-21 12:07:24 +0100

    Added annotations and texts to the Instruction panel

--- map-the-solar-system/server.R
+++ map-the-solar-system/server.R
@@ -1,22 +1,17 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+#Load all required libraries
+library(shiny) #duh
+library(leaflet) #for creating the map output
+library(lubridate) #for stripping apart the date of observation and feeding it into the planets() function
+library(moonsun) #for data on the positions of the planets in our solar system
+library(geosphere) #for calculating where the planets should be mapped to the earth, provided a scale
+library(data.table) #for providing the data.table output of the "raw" data that one can go out and use in a geocaching-esque way
+library(readbitmap) #for getting the planet icons' dimensions, mainly so that saturn does not get squashed into a square
 
-library(shiny)
-library(leaflet)
-library(lubridate)
-library(moonsun)
-library(geosphere)
-library(data.table)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    planetNames <- c("sun", 
+    #Defining constants in the beginning
+    AU <- 149597871 # this is one Astronomical Unit in km, which is based the average distance between Earth and the Sun.
+    planetNames <- c("sun", #I acknowledge that the Sun is not actually a planet, but a star. :) 
                      "mercury",
                      "venus",
                      "mars",
@@ -26,28 +21,62 @@ shinyServer(function(input, output) {
                      "neptune",
                      "pluto")
     
-    #To get the top-down-view from the ecliptic north to south poles
-    observationDate <- reactive({as.Date(input$date)})
-    planetScale <- reactive({as.numeric(input$kmPerAU)})
+    planetsOriginalSize <- 2 * c(695508, 2439.7, 6051.8, 3396.2, 71492, 60268, 25559, 24764, 1195)  #Source: NASA, radii converted to diameter in km, Earth has a radius of 6378.1
+    earthOriginalSize <- 6378.1 * 2
+    planetImages <- paste("www/", planetNames, ".png", sep="") #Pointing to the location of image files and constructing files names to be used as icons for map markers later on.
     
-    planetsAngles <- reactive({(-1) * (as.numeric(as.ecc(planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE))$lat) - 360)})
-    planetsDistances <- reactive({planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE)$dist * planetScale() * 1000})
-    planetsSize <- 2 * c(695508, 2439.7, 6051.8, 3396.2, 71492, 60268, 25559, 24764, 1195) # Source: NASA, radii converted to diameter
-        #Earth	6378.1
+    imageDims <- function(images) { #This constructs a function that can be used to get the width and height dimensions of a list of images, provided a filepath.
+        height <- NULL
+        width <- NULL
+        for(image in images) {
+        height <- c(height, dim(read.bitmap(image))[1])
+        width <- c(width, dim(read.bitmap(image))[2])
+        }
+        data.frame(height, width)
+    }
     
-    startingLocationLng <- reactive({as.numeric(input$startingLocationLng)})
+    planetImageDims <- imageDims(planetImages) #Applying the above function to get the dimensions of the planet icons / images.
+    
+    planetIconsHeight <- .02 * log(planetsOriginalSize) * planetImageDims$height #No auto-scaling here and I am also refraining from keeping sizes proportional, because this is not feasable using leaflet markers - the Sun is 100 times bigger than earth, for example; If you want to go out and map the planets to the world around you, check the data panel for properly scaled sizes in cm
+    planetIconsWidth <-  .02 * log(planetsOriginalSize) * planetImageDims$width
+    
+    planetIcons <- icons(iconUrl = planetImages, iconHeight = planetIconsHeight, iconWidth = planetIconsWidth)
+    earthIcon <- icons(iconUrl = "www/earth.png", iconHeight = .02 * log(earthOriginalSize), iconWidth = .02 * log(earthOriginalSize))
+    
+    
+    #Now, onto reactive values that can get set in the sidepanel by the user
+    observationDate <- reactive({as.Date(input$date)}) #This is needed to show how the planets are positioned on a given date
+    planetScale <- reactive({as.numeric(input$kmPerAU)}) #This is taking in the scale that the user wants to use to map the solar system to Earth's surface, in km per AU
+    
+    planetsAngles <- reactive({(-1) * (as.numeric(as.ecc(planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE))$lat) - 360)}) #This is getting the "angles", i.e. longitude variable from the planets() function call in the moonsun package. There seems to be an error in the output of the package, as I am having to grab the $lat variable to actually get the longitude in the eclicptic coordinate system; the "* (-1), - 360" part is to get the top-down-view, so from celestrial north to south, instead of south to north.
+    planetsDistances <- reactive({planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE)$dist * planetScale() * 1000}) #Grabbing the distances in AU from the planets() function call and converting them to meters, as the destPoint() function takes meters as input.
+    
+    planetsScaledSize <- reactive({((planetsOriginalSize * 1000 * 100) / AU) * planetScale()}) #converting km to m and then to cm
+    earthScaledSize <- reactive({((6378.1 * 2 * 1000 * 100) / AU) * planetScale()}) #see above, but for Earth seperately
+    
+    startingLocationLng <- reactive({as.numeric(input$startingLocationLng)}) #This is where Earth will be located, as the solar system will be mapped with Earth as its starting position.
     startingLocationLat <- reactive({as.numeric(input$startingLocationLat)})
     
-    newCoords <- reactive({destPoint(c(startingLocationLng(), startingLocationLat()), planetsAngles(), planetsDistances())})
+    newCoords <- reactive({destPoint(c(startingLocationLng(), startingLocationLat()), planetsAngles(), planetsDistances())}) #This calculates the coordinates of where to place the planets on the map with respect to the Earth.
     
-    output$solarSystemMap <- renderLeaflet({
+    dataTab <- reactive({data.table(Planet = planetNames, #This constructs a table of all data that is necessary to then go out into the world and place the planet models, as well as to know what size the models to build; you should probably refrain from building a to-scale model of the sun, as it will necessarily be huge, no matter what scale you choose.
+                                    Longitude = newCoords()[,1], 
+                                    Latitude = newCoords()[,2], 
+                                    "Distance in AU" = planetsDistances() / (1000 * planetScale()), 
+                                    "Diameter in km" = planetsOriginalSize, 
+                                    "Scaled distance in m" = planetsDistances(), 
+                                    "Scaled size in cm" = round(planetsScaledSize(), digits = 2)) %>%
+            rbind(list("earth", startingLocationLng(), startingLocationLat(), NA, 2 * 6378.1, NA, earthScaledSize()))
+            })
+                
+    output$solarSystemMap <- renderLeaflet({ #Rendering the actual map
         leaflet() %>%
         addTiles() %>%
-        addMarkers(lng = startingLocationLng(), lat = startingLocationLat(), label = "earth") %>%
-        addMarkers(lng = newCoords()[,1], lat = newCoords()[,2], label = planetNames)
+        addMarkers(lng = startingLocationLng(), lat = startingLocationLat(), label = "earth", icon = earthIcon) %>%
+        addMarkers(lng = newCoords()[,1], lat = newCoords()[,2], label = planetNames, icon = planetIcons)
     })
     
-    output$coordinatesDT <- renderDataTable({
-        data.table(Planet = planetNames, Longitude = newCoords()[,1], Latitude = newCoords()[,2], "Distance in AU" = planetsDistances() / ( 1000 * planetScale()), "Diameter in km"  = planetsSize)
+    output$coordinatesDT <- renderDataTable({ #Rendering the table with all the required data
+        dataTab()
     })
 })
--- map-the-solar-system/ui.R
+++ map-the-solar-system/ui.R
@@ -1,36 +1,35 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+#Loading the required packages
 library(shiny)
-library(leaflet)
 
 shinyUI(fluidPage(
   
-  # Application title
-  titlePanel("Map the Solar System Around You"),
+  titlePanel("The solar system around you"),
   
   #Sidebar layout with setting on the left and a map output to the right
   sidebarLayout(
     sidebarPanel(
        dateInput("date", "Date of observation"),
-       textInput("startingLocationLat", "Latitude, e.g. 35.0992539", value = "35.0992539"),
-       textInput("startingLocationLng", "Longitute, e.g. -115.7863069", value = "-115.7863069"),
+       p(strong("Where should Earth be located?")),
+       textInput("startingLocationLat", "Latitude", value = "35.0992539"),
+       textInput("startingLocationLng", "Longitude", value = "-115.7863069"),
        sliderInput("kmPerAU", "How much should 1 AU be in km?", value = 0.5, min = 0.01, max = 10, step = 0.01)
     ),
     
-    # Show a map of the location of stars relative to earth on a give day, with earth being at your position
+    # Show a map of the location of stars relative to earth on a give day, with Earth being at your position
     mainPanel(
         tabsetPanel(
-            tabPanel("mapPanel",
-                     img(src="earth.png"),
+            tabPanel("Instructions",
+                HTML("<h4>How to use this app</h4>
+                     <ul>
+                        <li><strong>Date of observation</strong><br>Since the planets move, you must pick the date for which you would like to map the current positions of planets, as well as the Sun.</li>
+                        <li><strong>Location of Earth</strong><br>The Earth acts as a central starting point for mapping the planets around it. By setting the longitude and latitude variables, you can define where to start your celestrial exploration. As a default, a location in the Mojave Desert is selected.</li></li>
+                        <li><strong>Date of observation</strong><br>Since the planets move, you must pick the date for which you would like to map the current positions of planets, as well as the Sun. By default, this date will be today's date.</li></li>
+                        <li><strong>Scale</strong><br>Distances in our Solar System are usually measured in AU (Astronomical Units). 1 AU is the average distance between the Sun and Earth, roughly 150 million km. By setting the slider on the left, you can decide how many kilometers 1 AU should represent. If you set this to 1, for example, the Sun will be placed about 1km away from earth and Pluto will be placed about 30km away from it.</li></li>
+                     </ul>")
+            ),
+            tabPanel("Map",
                      leafletOutput("solarSystemMap")),
-            tabPanel("dataPanel",
+            tabPanel("Data",
                      dataTableOutput("coordinatesDT"))
         )
     )
