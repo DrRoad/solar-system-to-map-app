c4c4a7cd254f1c82f51378e4d54b2f615b00731d
David Arens <davidarens@me.com>
2018-11-27 06:42:44 +0100

    done for now

--- map-the-solar-system/server.R
+++ map-the-solar-system/server.R
@@ -6,7 +6,7 @@ library(moonsun) #for data on the positions of the planets in our solar system
 library(geosphere) #for calculating where the planets should be mapped to the earth, provided a scale
 library(data.table) #for providing the data.table output of the "raw" data that one can go out and use in a geocaching-esque way
 library(readbitmap) #for getting the planet icons' dimensions, mainly so that saturn does not get squashed into a square
-
+library(Hmisc) #For text operations
 
 shinyServer(function(input, output) {
     #Defining constants in the beginning
@@ -41,8 +41,22 @@ shinyServer(function(input, output) {
     planetIconsWidth <-  .02 * log(planetsOriginalSize) * planetImageDims$width
     
     planetIcons <- icons(iconUrl = planetImages, iconHeight = planetIconsHeight, iconWidth = planetIconsWidth)
-    earthIcon <- icons(iconUrl = "www/earth.png", iconHeight = .02 * log(earthOriginalSize), iconWidth = .02 * log(earthOriginalSize))
+    earthIcon <- icons(iconUrl = "www/earth.png", iconHeight = .02 * log(earthOriginalSize) * 120, iconWidth = .02 * log(earthOriginalSize) * 120)
     
+    makePlanetPopups <- function(name, image, size, scaledSize, distance, scaledDistance) {
+                        paste("<strong>",
+                            capitalize(name),
+                          "</strong> <br> <img src=\"",
+                            image,
+                          "\" height = 92px><br>Actual diameter in km: ",
+                          size,
+                          "<br>Scaled size in cm: ",
+                          scaledSize,
+                          "<br>Actual distance in AU: ",
+                          distance,
+                          "<br>Scaled distance in m: ",
+                          scaledDistance, sep = "")
+    }
     
     #Now, onto reactive values that can get set in the sidepanel by the user
     observationDate <- reactive({as.Date(input$date)}) #This is needed to show how the planets are positioned on a given date
@@ -50,30 +64,34 @@ shinyServer(function(input, output) {
     
     planetsAngles <- reactive({(-1) * (as.numeric(as.ecc(planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE))$lat) - 360)}) #This is getting the "angles", i.e. longitude variable from the planets() function call in the moonsun package. There seems to be an error in the output of the package, as I am having to grab the $lat variable to actually get the longitude in the eclicptic coordinate system; the "* (-1), - 360" part is to get the top-down-view, so from celestrial north to south, instead of south to north.
     planetsDistances <- reactive({planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE)$dist * planetScale() * 1000}) #Grabbing the distances in AU from the planets() function call and converting them to meters, as the destPoint() function takes meters as input.
+    planetsDistanceAU <- reactive({planets(jday = jd(year = year(observationDate()), month = month(observationDate()), day = day(observationDate())), show.moon = FALSE)$dist})
     
-    planetsScaledSize <- reactive({((planetsOriginalSize * 1000 * 100) / AU) * planetScale()}) #converting km to m and then to cm
-    earthScaledSize <- reactive({((6378.1 * 2 * 1000 * 100) / AU) * planetScale()}) #see above, but for Earth seperately
+    planetsScaledSize <- reactive({round(((planetsOriginalSize * 1000 * 100) / AU) * planetScale(), digits = 2)}) #converting km to m and then to cm
+    earthScaledSize <- reactive({round(((6378.1 * 2 * 1000 * 100) / AU) * planetScale(), digits = 2)}) #see above, but for Earth seperately
+    
+    planetPopups <- reactive({mapply(makePlanetPopups, planetNames, paste(planetNames, ".png", sep = ""), planetsOriginalSize, planetsScaledSize(), planetsDistanceAU(), planetsDistances(), USE.NAMES = FALSE)}) #Create the popups for the planets and the Sun
+    earthPopup <- reactive({paste("<strong>Earth</strong><br><img src = \"earth.png\"><br>Actual diameter in km: ", earthOriginalSize,"<br>Scaledd size in cm: ", earthScaledSize())})
     
     startingLocationLng <- reactive({as.numeric(input$startingLocationLng)}) #This is where Earth will be located, as the solar system will be mapped with Earth as its starting position.
     startingLocationLat <- reactive({as.numeric(input$startingLocationLat)})
     
     newCoords <- reactive({destPoint(c(startingLocationLng(), startingLocationLat()), planetsAngles(), planetsDistances())}) #This calculates the coordinates of where to place the planets on the map with respect to the Earth.
     
-    dataTab <- reactive({data.table(Planet = planetNames, #This constructs a table of all data that is necessary to then go out into the world and place the planet models, as well as to know what size the models to build; you should probably refrain from building a to-scale model of the sun, as it will necessarily be huge, no matter what scale you choose.
+    dataTab <- reactive({data.table(Planet = capitalize(planetNames), #This constructs a table of all data that is necessary to then go out into the world and place the planet models, as well as to know what size the models to build; you should probably refrain from building a to-scale model of the sun, as it will necessarily be huge, no matter what scale you choose.
                                     Longitude = newCoords()[,1], 
                                     Latitude = newCoords()[,2], 
                                     "Distance in AU" = planetsDistances() / (1000 * planetScale()), 
                                     "Diameter in km" = planetsOriginalSize, 
                                     "Scaled distance in m" = planetsDistances(), 
-                                    "Scaled size in cm" = round(planetsScaledSize(), digits = 2)) %>%
-            rbind(list("earth", startingLocationLng(), startingLocationLat(), NA, 2 * 6378.1, NA, earthScaledSize()))
+                                    "Scaled size in cm" = planetsScaledSize()) %>%
+            rbind(list("Earth", startingLocationLng(), startingLocationLat(), NA, 2 * 6378.1, NA, earthScaledSize()))
             })
                 
     output$solarSystemMap <- renderLeaflet({ #Rendering the actual map
         leaflet() %>%
         addTiles() %>%
-        addMarkers(lng = startingLocationLng(), lat = startingLocationLat(), label = "earth", icon = earthIcon) %>%
-        addMarkers(lng = newCoords()[,1], lat = newCoords()[,2], label = planetNames, icon = planetIcons)
+        addMarkers(lng = startingLocationLng(), lat = startingLocationLat(), label = "Earth", icon = earthIcon, popup = earthPopup()) %>%
+        addMarkers(lng = newCoords()[,1], lat = newCoords()[,2], label = capitalize(planetNames), popup = planetPopups(), icon = planetIcons)
     })
     
     output$coordinatesDT <- renderDataTable({ #Rendering the table with all the required data
--- map-the-solar-system/ui.R
+++ map-the-solar-system/ui.R
@@ -1,16 +1,10 @@
-#TODO:
-# - Add acknowledgements
-# - Add popups to map markers
-# - Publish
-
-
 #Loading the required packages
 library(shiny)
 library(leaflet)
 
 shinyUI(fluidPage(
   
-  titlePanel("The solar system around you"),
+  titlePanel("Solar System Mapper"),
   
   #Sidebar layout with setting on the left and a map output to the right
   sidebarLayout(
@@ -19,7 +13,8 @@ shinyUI(fluidPage(
        p(strong("Where should Earth be located?")),
        textInput("startingLocationLat", "Latitude", value = "35.0992539"),
        textInput("startingLocationLng", "Longitude", value = "-115.7863069"),
-       sliderInput("kmPerAU", "How much should 1 AU be in km?", value = 0.5, min = 0.01, max = 10, step = 0.01)
+       sliderInput("kmPerAU", "How much should 1 AU be in km?", value = 0.5, min = 0.01, max = 10, step = 0.01),
+       submitButton()
     ),
     
     # Show a map of the location of stars relative to earth on a give day, with Earth being at your position
@@ -27,12 +22,11 @@ shinyUI(fluidPage(
         tabsetPanel(
             tabPanel("Instructions",
                 HTML("<h4>How to use this app</h4>
-                    <p>The purpose of this app is to let anyone experience the scale of our solar system. Based on a location chosen by the user, as well as a given date and scale measure, it calculates the positions of all planets, plus Pluto and the Sun as viewed from a bird's eye perspective. The user can set the following variables:</p>
+                    <p>The purpose of this app is to let anyone experience the scale of our solar system. Based on a geographical location chosen by the user, as well as a given date and scale, it calculates the positions of all planets (plus Pluto and the Sun) on the map as viewed from a bird's eye perspective. The user can set the following variables:</p>
                      <ul>
                         <li><strong>Date of observation</strong><br>Since the planets move, you must pick the date for which you would like to map the current positions of planets, as well as the Sun.</li>
                         <li><strong>Location of Earth</strong><br>The Earth acts as a central starting point for mapping the planets around it. By setting the longitude and latitude variables, you can define where to start your celestrial exploration. As a default, a location in the Mojave Desert is selected.</li></li>
-                        <li><strong>Date of observation</strong><br>Since the planets move, you must pick the date for which you would like to map the current positions of planets, as well as the Sun. By default, this date will be today's date.</li></li>
-                        <li><strong>Scale</strong><br>Distances in our Solar System are usually measured in AU (Astronomical Units). 1 AU is the average distance between the Sun and Earth, roughly 150 million km. By setting the slider on the left, you can decide how many kilometers 1 AU should represent. If you set this to 1, for example, the Sun will be placed about 1km away from earth and Pluto will be placed about 30km away from it.</li></li>
+                        <li><strong>Scale</strong><br>Distances in our Solar System are usually measured in AU (Astronomical Units). 1 AU is the average distance between the Sun and Earth, roughly 150 million km. By setting the slider on the left, you can decide how many kilometers 1 AU should represent. If you set this to 1, for example, the Sun will be placed about 1km away from Earth and Pluto will be placed about 30km away from it.</li></li>
                      </ul>")
             ),
             tabPanel("Map",
@@ -40,21 +34,23 @@ shinyUI(fluidPage(
             tabPanel("Data",
                      dataTableOutput("coordinatesDT")),
             tabPanel("Acknowledgements",
-                     HTML("<p>This app was inspired by the National Geographic TV show MARS. In season 1, episode 2, young Ben Sawyer - one of the astronauts destined for Mars - goes out into the desert with his dad to place marbles on sticks, so that the marbles' size would match the size of the planets in our solar system and the distance between them would match the distance between the planets on the same scale. You can read more about this on <a href='https://medium.com/p/a5f7f010ffac/'>Medium</a>. The code for this app is available on <a href='https://github.com/pdwarf/solar-system-to-map-app/tree/master/map-the-solar-system'>github</a>.</p>") #Add images and thanks to packages' authors here.
+                     HTML("<p>This app was inspired by the <a href=\"https://www.nationalgeographic.com/tv/mars/\">National Geographic TV show MARS</a>. In season 1, episode 2, young Ben Sawyer - one of the astronauts destined for Mars - goes out into the desert with his dad to place marbles on sticks, so that the marbles' size would match the size of the planets in our solar system and the distance between them would match the distance between the planets on the same scale. You can read more about this on <a href='https://medium.com/p/a5f7f010ffac/'>Medium</a>. The code for this app is available on <a href='https://github.com/pdwarf/solar-system-to-map-app/tree/master/map-the-solar-system'>github</a>.</p>"),
+                     img(src="https://github.com/pdwarf/solar-system-to-map-app/blob/master/map-the-solar-system/www/MARS_earth_marble.png?raw=true", width="600px"),
+                     HTML("Images from the following sources were used:<br>
+                          <ul>
+                            <li>earth: http://pngimg.com/download/25352</li>
+                            <li>sun: http://pngimg.com/download/13424</li>
+                            <li>mars: https://commons.wikimedia.org/wiki/File:3D_Mars.png</li>
+                            <li>venus: https://commons.wikimedia.org/wiki/File:3D_Venus.png</li>
+                            <li>uranus: https://commons.wikimedia.org/wiki/File:3D_Uranus.png</li>
+                            <li>jupiter: https://commons.wikimedia.org/wiki/File:3D_Jupiter.png</li>
+                            <li>mercury: https://commons.wikimedia.org/wiki/File:3D_Mercury.png</li>
+                            <li>saturn: https://commons.wikimedia.org/wiki/File:3D_Saturn.png</li>
+                            <li>neptune: https://commons.wikimedia.org/wiki/File:3D_Neptune.png</li>
+                            <li>pluto: https://commons.wikimedia.org/wiki/File:3D_Pluto.png</li>
+                          </ul>")
                      )
         )
     )
   )
 ))
-
-# images:
-#     earth: http://pngimg.com/download/25352
-#     sun: http://pngimg.com/download/13424
-#     mars: https://commons.wikimedia.org/wiki/File:3D_Mars.png
-#     venus: https://commons.wikimedia.org/wiki/File:3D_Venus.png
-#     uranus: https://commons.wikimedia.org/wiki/File:3D_Uranus.png
-#     jupiter: https://commons.wikimedia.org/wiki/File:3D_Jupiter.png
-#     mercury: https://commons.wikimedia.org/wiki/File:3D_Mercury.png
-#     saturn: https://commons.wikimedia.org/wiki/File:3D_Saturn.png
-#     neptune: https://commons.wikimedia.org/wiki/File:3D_Neptune.png
-#     pluto: https://commons.wikimedia.org/wiki/File:3D_Pluto.png
